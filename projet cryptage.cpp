a6912eb563cae9225757d8e0cbc6990c34ffe505
TubertBenjamin <144659775+TubertBenjamin@users.noreply.github.com>
2023-12-07 10:08:47 +0000

    Partie 1 cryptage

--- projet cryptage.cpp
+++ projet cryptage.cpp
@@ -5,16 +5,28 @@ using namespace std;
 
 int main() {
 int i =0;
-char tab[30];
-char inverse[30];
+char tab[30] ="";
+char inverse[30] ="";
+char sortie[30] ;
+int decal=2;
 
 cout << "Entrez une phrase" << endl;
+
 cin.getline(tab,30);  
+
 int longueur = strlen(tab);
+
 for(i=0;i<longueur+1;i++){
     inverse[i-1]=tab[longueur -i]; 
+    if ((inverse[i] >= 'A' && inverse[i] <= 'Z')) { // Si le ième caractère est compris entre ‘A' et ‘Z'
+            sortie[i] = (((inverse[i] - 65) + decal) % 26) + 65; // inverse[i] -65 +2 
+        } else if ((inverse[i] >= 'a' && inverse[i] <= 'z')) { // 
+            sortie[i] = (((inverse[i] - 97) + decal) % 26) + 97; //
+        }
 }
 
+
 cout << "Votre phrase est " << inverse << endl; 
+cout << "Votre phrase est " << sortie << endl; 
 return 0; 
 }
